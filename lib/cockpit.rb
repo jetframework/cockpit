d5c4f67091eefe4391837cd76b17a5d8431fa7b8
maclover7 <me@jonathanmoss.me>
2015-09-07 10:24:33 -0400

    initial commit

---
+++ lib/cockpit.rb
@@ -0,0 +1,4 @@
+require "cockpit/version"
+
+module Cockpit
+end
---
+++ spec/cockpit_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Cockpit do
+  it 'has a version number' do
+    expect(Cockpit::VERSION).not_to be nil
+  end
+end
